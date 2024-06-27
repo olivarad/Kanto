0760f38c738dd71a2ee523c1b390fffc5367aecd
olivarad <olivia.radecki02@gmail.com>
2024-06-27 16:28:20 -0400

    Help message for swapParty command added

--- definitions.py
+++ definitions.py
@@ -2,4 +2,5 @@ commands = f"Commands: \
                         \n!commands: See commands \
                         \n!ready: Begin your pokemon journey (can not be used more than once) \
                         \n!starter: Indicate that you would like to choose your starter pokemon \
-                        \n!viewParty: View your party"
+                        \n!viewParty: View your party \
+                        \n!swapParty: Swap 2 party slots (!swapParty slot1 slot2)"
