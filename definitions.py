ac5a8542990b086f10d3ecec115c982e1b5b9499
olivarad <olivia.radecki02@gmail.com>
2024-06-27 23:31:04 -0400

    Naming scheme standardization

--- definitions.py
+++ definitions.py
@@ -2,7 +2,7 @@ commands = f"Commands: \
                         \n!commands: See commands \
                         \n!ready: Begin your pokemon journey (can not be used more than once) \
                         \n!starter: Indicate that you would like to choose your starter pokemon \
-                        \n!viewParty: View your party \
+                        \n!showParty: View your party \
                         \n!swapParty: Swap 2 party slots (!swapParty slot1 slot2)\
                         \n!showBadges: Display your badges \
                         \n!showInventory: Display your inventory"
